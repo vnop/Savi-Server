7347421a0e6a220a0bb6261672e5fb837d8e10b3
Alex Moores <moores.alexd@gmail.com>
2017-04-08 19:40:02 -0400

    added more info for config_TEMPLATE.js

--- config/config_TEMPLATE.js
+++ config/config_TEMPLATE.js
@@ -2,3 +2,10 @@ module.exports = {
   dbName: 'savitravel_alex',
   port: 8081
 };
+
+// Ports: 8081 - Alex
+//        8082 - Seán
+//        8083 - Vandeth
+//        8084 - Ignacio
+//        8080 - staging
+//        443 - live

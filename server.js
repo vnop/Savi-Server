54fd6fb1a221148fbdaa873483849a088fec992f
Alex Moores <moores.alexd@gmail.com>
2017-04-12 22:17:14 -0400

    testing logging of booking var

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.get('/api/bookings', (req, res) => {
             });
 
             Promise.all([findDriver, findGuide]).then(() => {
+              console.log(booking);
               if (booking.guide && booking.driver) {
                 res.json(booking).end();
               } else {

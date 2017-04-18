3e9663b593a14a83156f0fae6d629e58c6aaa764
Ignacio Palma <naxopalma@gmail.com>
2017-04-18 17:54:26 -0300

    email testing

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ const app = express();
 
 require('./routes.js')(app, express, db);
 
-// var pKey = fs.readFileSync('/etc/letsencrypt/live/savi-travel.com/privkey.pem');
-// var cert = fs.readFileSync('/etc/letsencrypt/live/savi-travel.com/fullchain.pem');
-// var ca = fs.readFileSync('/etc/letsencrypt/live/savi-travel.com/chain.pem');
+var pKey = fs.readFileSync('/etc/letsencrypt/live/savi-travel.com/privkey.pem');
+var cert = fs.readFileSync('/etc/letsencrypt/live/savi-travel.com/fullchain.pem');
+var ca = fs.readFileSync('/etc/letsencrypt/live/savi-travel.com/chain.pem');
 
-// let server = https.createServer({key: pKey, cert: cert, ca: ca}, app);
+let server = https.createServer({key: pKey, cert: cert, ca: ca}, app);
 
-// server.listen(config.port, () => {console.log('listening on port', config.port)});
+server.listen(config.port, () => {console.log('listening on port', config.port)});
 
-app.listen(config.port, () => {console.log('listening on port... ' + config.port)});
+//app.listen(config.port, () => {console.log('listening on port... ' + config.port)});
 
 

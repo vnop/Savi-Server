39507185ee267c1d86a1e4f7daa403dfec01223e
nihilates <nihilates@gmail.com>
2017-04-18 21:12:00 -0400

    Working on api calls

--- routes.js
+++ routes.js
@@ -182,7 +182,7 @@ module.exports = function(app, express, db) {
 		}
 	});
 
-	app.get('/*', (req, res) => {
+	app.get('/addcity', (req, res) => {
 		res.sendFile(__dirname, '/panel/index.html');
 	});
 }

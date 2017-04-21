96a36721042231acf2414f39085168f3b71f3cbf
nihilates <nihilates@gmail.com>
2017-04-21 14:07:59 -0400

    Further API/DB testing

--- panel/app/App.jsx
+++ panel/app/App.jsx
@@ -35,7 +35,7 @@ export default class App extends React.Component {
                   </span>
 
                   <span id="navItem" className="nav-item">
-                    <Link to="/manageusers">Users</Link>
+                    <Link to="/manageusers">Manage Users</Link>
                   </span>
 
                 </div>

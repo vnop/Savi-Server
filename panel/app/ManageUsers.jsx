7419f77f18f5c0a8e11481df8faa7b97b4eaa6a0
nihilates <nihilates@gmail.com>
2017-04-21 21:25:15 -0400

    Building the manage user page

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -16,11 +16,14 @@ class AddCity extends React.Component {
 
     //METHOD BINDINGS
     this.methodMenu = this.methodMenu.bind(this);
+    this.nameForm = this.nameForm.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   //FORM CONTROLS
   methodMenu(e) {this.setState({ method: e.target.value })}
+  nameForm(e) {this.setState({ method: e.target.value })}
+
   handleSubmit(e) {
     e.preventDefault();
     console.log('LOGGING', this.state.method);
@@ -30,7 +33,7 @@ class AddCity extends React.Component {
     return array.filter((obj) => {
       return obj[key] === val;
     });
-  } 
+  }
 
   render() {
     return (
@@ -42,17 +45,16 @@ class AddCity extends React.Component {
               <option value="userName">User Name</option>
               <option value="userEmail">Email</option>
               <option value="mdn">Phone Number</option>
-              <option value="city">City</option>              
+              <option value="city">City</option>
             </select>
           </label>
 
           <label>
-            {(this.method==="userName") ? ()=>{
-              return (<div>Search By User Name</div>)} :
-              ()=>{
-                return (<div>Search By SOMETHING ELSE</div>)
-              }
-            }
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.nameForm}
+            />
           </label>
 
           <input type="submit" value="Search" />

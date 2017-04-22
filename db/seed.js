7cec7f48cc9f4955bf67e8162cf8df31f109d8ec
Alex Moores <moores.alexd@gmail.com>
2017-04-21 20:14:04 -0400

    seed does not do that

--- db/seed.js
+++ db/seed.js
@@ -4,6 +4,5 @@ const config = require('../config/config');
 const seeder = require('./seeder.js');
 const schema = new Sequelize(config.dbName, 'root', config.password);
 
-schema.drop().then(() => {
-  db.syncTables(true, schema).then(() => {seeder.seedDatabase(db)});
-});
+
+db.syncTables(true, schema).then(() => {seeder.seedDatabase(db)});

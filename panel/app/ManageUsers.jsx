b920bd08c5740e596305ed4d153ac872244de44e
nihilates <nihilates@gmail.com>
2017-04-22 18:10:20 -0400

    Working on dynamic display of search results

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -132,53 +132,54 @@ class DynamicForms extends React.Component {
   }
 
   render() {
-    //check for the value of the props.method to determine which form to render
-    <div>{
-      if (this.props.method==='userName') {//if the search method is by userName
-        return (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" value={this.state.userName} onChange={this.nameForm} />
-              <input type="submit" value="Search" />
-            </form>
-          </div>
-        )
-      } else if (this.props.method==='userEmail') {//if the search method is by userEmail
-        return (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" value={this.state.userEmail} onChange={this.emailForm} />
-              <input type="submit" value="Search" />
-            </form>
-          </div>
-        )
-      } else if (this.props.method==='mdn') {//if the search method is by mdn (mobile device number)
-        return (
-           <div>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" value={this.state.mdn} onChange={this.mdnForm} />
-              <input type="submit" value="Search" />
-            </form>
-          </div>
-        )
-      } else if (this.props.method==='cityId') {//if the search method is by userName
-        return (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <select onChange={this.cityForm} value={this.state.cityId}>
-                <option value={0}>*all</option>
-                {this.props.cityData.map((item, i) => {
-                  return (
-                    <option key={i} value={item.id}>{item.name}</option>
-                  )
-                })}
-              </select>
-              <input type="submit" value="Search" />
-            </form>
-          </div>
-        )
-      }
-    }</div>
+    return (
+      <div>{
+        if (this.props.method==='userName') {
+          return (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.userName} onChange={this.nameForm} />
+                <input type="submit" value="Search" />
+              </form>
+            </div>
+          )
+        } else if (this.props.method==='userEmail') {
+          return (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.userEmail} onChange={this.emailForm} />
+                <input type="submit" value="Search" />
+              </form>
+            </div>
+          )
+        } else if (this.props.method==='mdn') {
+          return (
+             <div>
+              <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.mdn} onChange={this.mdnForm} />
+                <input type="submit" value="Search" />
+              </form>
+            </div>
+          )
+        } else if (this.props.method==='cityId') {
+          return (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <select onChange={this.cityForm} value={this.state.cityId}>
+                  <option value={0}>*all</option>
+                  {this.props.cityData.map((item, i) => {
+                    return (
+                      <option key={i} value={item.id}>{item.name}</option>
+                    )
+                  })}
+                </select>
+                <input type="submit" value="Search" />
+              </form>
+            </div>
+          )
+        }
+      }</div>
+    )
   }//End of Render
 }
 

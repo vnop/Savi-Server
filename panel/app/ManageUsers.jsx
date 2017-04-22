26dd44ccee4b6969c9ae3f09c67fcfe425e3bc2d
nihilates <nihilates@gmail.com>
2017-04-22 13:43:42 -0400

    Building the manage user page

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -35,6 +35,7 @@ class DynamicForms extends React.Component {
   render() {
     //check for the value of the props.method to determine which form to render
     if (this.props.method==='userName') {//if the search method is by userName
+      this.setState({ userName: '', userEmail: '', mdn: '', cityId: '' });
       return (
         <form onSubmit={this.handleSubmit}>BY USERNAME
           <input type="text" value={this.state.userName} onChange={this.nameForm} />

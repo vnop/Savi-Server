ed723d512d1b0c083c0bae46cff13ed59bb48006
nihilates <nihilates@gmail.com>
2017-04-22 17:44:40 -0400

    Working on dynamic display of search results

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -39,7 +39,7 @@ class ManageUsers extends React.Component {
           </select>
         </form>
         <DynamicForms method={this.state.method} cityData={this.state.cityData} />
-        <DisplayUsers/>
+        <DisplayUsers data={window.data} />
       </div>
     )
   }//End of Render

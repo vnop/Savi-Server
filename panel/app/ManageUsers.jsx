926569b01508a03cbbc97d48688d76708b586949
nihilates <nihilates@gmail.com>
2017-04-21 21:08:10 -0400

    Building the manage user page

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -7,18 +7,47 @@ class AddCity extends React.Component {
     this.state = {
       data: [],
       //form field states
-      userName: '',
-      userEmail: '',
-      mdn: '',
+      method: 'userName',
+      name: '',
+      email: '',
+      mdn: 0, //mobile device number
       country: '',
-      userAuthId: ''
+      city: ''
     };
+
+    //METHOD BINDINGS
+    this.methodMenu = this.methodMenu.bind(this);
   }
+
+  //FORM CONTROLS
+  methodMenu(e) {this.setState({ method: e.target.value })}
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('LOGGING', this.state.method);
+  }
+
+  processData(array, key, val) { //takes and array, a key (as string), and value
+    return array.filter((obj) => {
+      return obj[key] === val;
+    });
+  } 
 
   render() {
     return (
       <div>
-        MANAGE USERS
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search By:
+            <select onChange={this.methodForm} value={this.state.method}>
+              <option value="userName">User Name</option>
+              <option value="userEmail">Email</option>
+              <option value="mdn">Phone Number</option>
+              <option value="country">Country</option>
+              <option value="city">City</option>              
+            </select>
+          </label>
+          <input type="submit" value="Add" />
+        </form>
       </div>
     )
   }

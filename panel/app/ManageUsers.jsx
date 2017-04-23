11569f1a542123750039e3ce8d951b4513eed76f
nihilates <nihilates@gmail.com>
2017-04-22 20:39:21 -0400

    Working on dynamic display of search results

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -255,7 +255,7 @@ class UserData extends React.Component {
           } else {
             return (
               <div>
-                Let's Save
+                {this.state.userName}
               </div>
             )
           }

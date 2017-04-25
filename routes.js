4332de3435109652cd6b5b1236075737ae1043b5
Ignacio Palma <naxopalma@gmail.com>
2017-04-25 15:22:55 -0300

    stripe post json 6

--- routes.js
+++ routes.js
@@ -22,7 +22,7 @@ module.exports = function(app, express, db, log) {
 	app.use(bodyParser.urlencoded({ extended: true }));
 
 	app.post('/payments', function(req, res){
-	  	console.log('payment request..', req.body)
+	  	console.log('payment request..', req)
 	  	res.redirect('/');
 	});
 

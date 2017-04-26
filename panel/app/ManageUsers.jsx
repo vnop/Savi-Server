7fe3ba6fa1f3b13e3798d104982b271f280a77e4
nihilates <nihilates@gmail.com>
2017-04-26 18:17:02 -0400

    hooking up employee API to panel

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -97,11 +97,8 @@ class DynamicForms extends React.Component {
     this.props.callback([]);//Reset the "Data" state in ManageUsers before updating it
     //GET request for the input data
     fetch('https://savi-travel.com:'+config.port+'/api/users'+searchTerm(), {mode: 'no-cors'})
-      .then(resp => {resp.json()})
-      .then(data => {
-      this.props.callback(data);
-      console.log("DATA SEND")
-      })//sends the data up to the ManageUsers component
+      .then(resp => resp.json())
+      .then(data => this.props.callback(data))//sends the data up to the ManageUsers component
       .catch(err => console.error(err));
   }
 

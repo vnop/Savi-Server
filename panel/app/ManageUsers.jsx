80c4b37de31094476d6f7908f93cd1403bb0387e
nihilates <nihilates@gmail.com>
2017-04-26 13:58:11 -0400

    hooking up employee API to panel

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -251,6 +251,7 @@ class UserData extends React.Component {
 
   saveHandler(e) {
     if (this.props.data.type !== this.state.type) {//if the original props "type" doesn't match the state "type"...
+      console.log("IF THIS.STATE.TYPE", this.state.type)
       if (this.state.type === "Tourist") {//if the new type is "Tourist"...
         //delete the employee entry for this userId
         fetch('https://savi-travel.com:'+config.port+'/api/employees', {

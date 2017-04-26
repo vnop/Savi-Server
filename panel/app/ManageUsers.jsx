af5760a6680aba6395ebd0fe3d64a85971167924
nihilates <nihilates@gmail.com>
2017-04-25 23:36:51 -0400

    hooking up employee API to panel

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -297,7 +297,7 @@ class UserData extends React.Component {
                   <select onChange={this.cityForm} value={this.state.city}>
                     {this.props.cityData.map((item, i) => {
                       return (
-                        <option key={i} value={{id: item.id}}>{item.name}</option>
+                        <option key={i} value={item.id}>{item.name}</option>
                       )
                     })}
                   </select>

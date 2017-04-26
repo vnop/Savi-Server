f80a4e94b0875cd01ba756cffc5f250242e604a9
Alex Moores <moores.alexd@gmail.com>
2017-04-26 16:14:46 -0400

    typo

--- mailer/mail_template.js
+++ mailer/mail_template.js
@@ -36,6 +36,7 @@ module.exports = {
 							'<h3>You have the '+ tourName +' tour booked</h3>' +
 							'<h2>The date of the tour is '+ date +'</h2>' +
 							'<h2>Enjoy the experience!</h2>'
+		}
 	}
 }
 

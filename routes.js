b5c8a9737350faf0c0c64949ba84e0519b026a23
nihilates <nihilates@gmail.com>
2017-04-26 17:00:18 -0400

    hooking up employee API to panel

--- routes.js
+++ routes.js
@@ -446,6 +446,7 @@ module.exports = function(app, express, db, log) {
 		  //get the cityId from the City table
 			db.City.find({where: {name: req.body.city}}).then((city) => {
 				if (!!city) {//if the city is found...
+					console.log("CITY FOUND")
 					let employ = {//store inbound employee data in employ object
 				    userId: req.body.userId,
 				    cityId: city.dataValues.id,
@@ -456,6 +457,7 @@ module.exports = function(app, express, db, log) {
 				  //first, check to see if an employee entry exists already
 				  db.EmployeeData.find({where: {userId: employ.userId}}).then((employee) => {
 				  	if (!employee) {//if such an employee already exists...
+				  		console.log("EMPLOYEE FOUND")
 				  		db.EmployeeData.create(employ).then((employee) => {//create a new entry in the employee database
 				  			res.json({exists: true, employee: employee}).end();
 				  		}).catch((err) => {//error handling

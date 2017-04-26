9e7992ba030350b84ea926dbc6720d6c45aacfa1
Alex Moores <moores.alexd@gmail.com>
2017-04-26 16:11:15 -0400

    fixed missing mail template

--- mailer/mail_template.js
+++ mailer/mail_template.js
@@ -5,8 +5,8 @@ module.exports = {
 			return  '<h1>Hello '+ name +'</h1>' +
 							'<h3>You have the '+ tourName +' tour booked</h3>' +
 							'<h2>The date of the tour is '+ date +'</h2>' +
-							'<h2>Enjoy the experience!</h2>' 					
-		}		
+							'<h2>Enjoy the experience!</h2>'
+		}
 	},
 
 	'Tour Guide': {
@@ -15,8 +15,8 @@ module.exports = {
 			return  '<h1>Dear Tour Guide '+ name +'</h1>' +
 							'<h3>You have the '+ tourName +' tour booked</h3>' +
 							'<h2>The date of the tour is '+ date +'</h2>' +
-							'<h2>Enjoy the experience!</h2>' 					
-		}		
+							'<h2>Enjoy the experience!</h2>'
+		}
 	},
 
 	'Driver': {
@@ -25,8 +25,16 @@ module.exports = {
 			return  '<h1>Dear Driver '+ name +'</h1>' +
 							'<h3>You have the '+ tourName +' tour booked</h3>' +
 							'<h2>The date of the tour is '+ date +'</h2>' +
-							'<h2>Enjoy the experience!</h2>' 					
-		}		
+							'<h2>Enjoy the experience!</h2>'
+		}
+	}
+	'Tourist': {
+		subject: 'You have a new tour booked! ✔',
+		content: function(name, tourName, date) {
+			return  '<h1>Dear Customer '+ name +'</h1>' +
+							'<h3>You have the '+ tourName +' tour booked</h3>' +
+							'<h2>The date of the tour is '+ date +'</h2>' +
+							'<h2>Enjoy the experience!</h2>'
 	}
 }
-	
+

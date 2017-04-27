2c0858c90e33fdd69febbe8dd9c41ee03a7fbf91
nihilates <nihilates@gmail.com>
2017-04-26 20:01:58 -0400

    hooking up employee API to panel

--- panel/app/ManageUsers.jsx
+++ panel/app/ManageUsers.jsx
@@ -382,7 +382,7 @@ class UserData extends React.Component {
                       <div>
                       Available Seats:
                         <select onChange={this.seatsForm} value={this.state.seats}>
-                          {[1,2,3,4,5,6,7,8,9,10].map((item, i) => {
+                          {[0,1,2,3,4,5,6,7,8,9,10].map((item, i) => {
                             <option key={i} value={item}>item</option>
                           })}
                         </select>

390c2344e112d6b6f052702a97a3b60b94fea5bb
Alex Moores <moores.alexd@gmail.com>
2017-04-26 21:57:07 -0400

    updating readme

--- config/config_TEMPLATE.js
+++ config/config_TEMPLATE.js
@@ -1,11 +1,8 @@
 module.exports = {
-  dbName: 'savitravel_ignacio',
-  port: 8084
+  port: 0, // Port number for deployment
+  dbName: '~~dbName for mysql~~',
+  uname: '~~username for mysql~~',
+  password: '~~password for mysql~~',
+  pKeyPath: '~~path to private key~~',
+  certPath: '~~path to certificate~~'
 };
-
-// Ports: 8081 - Alex
-//        8082 - Seán
-//        8083 - Vandeth
-//        8084 - Ignacio
-//        8080 - staging
-//        443 - live

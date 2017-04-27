36c9668a6c566eed97b869bd542ed674cc6b02aa
Ignacio Palma <naxopalma@gmail.com>
2017-04-27 16:20:54 -0300

    login

--- panel/app/Login.jsx
+++ panel/app/Login.jsx
@@ -40,12 +40,16 @@ class Login extends React.Component {
   render() {
     return loggedIn ? (<Redirect to="/"/>) :
     (
-      <div>
-        <form onSubmit={this.attemptSignin}>
-          Username: <input type="text" ref="uName"></input>
-          Password: <input type="password" ref="pWord"></input>
-          <button type="submit">Submit</button>
-        </form>
+      <div className="login">
+        <div className="login-form-wrapper">
+          <form onSubmit={this.attemptSignin}>
+            <p>Username</p>
+            <input type="text" ref="uName"></input>
+            <p>Password</p>
+            <input type="password" ref="pWord"></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }

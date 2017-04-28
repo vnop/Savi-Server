0363e5ff44ec0b5c0c2d3e616c6fd6decb1469a9
nihilates <nihilates@gmail.com>
2017-04-27 21:18:06 -0400

    Refractor of routes

--- routes/imagesRoutes.js
+++ routes/imagesRoutes.js
@@ -6,11 +6,10 @@ module.exports = function(app, db) {
 
   app.get('/api/images/:imageName', (req, res) => {
     let imageName = req.params.imageName;
-    let exists = fs.existsSync(path.join(__dirname, '/img/' + imageName));
+    let exists = fs.existsSync(path.join(__dirname, '/../img/' + imageName));
     if (imageName && exists) {
       res.sendFile(path.join(__dirname, '/img/' + imageName));
     } else if (!exists) {
-      console.log(__dirname, '/../img/'+imageName)
       res.status(404).send(JSON.stringify('Image does not exist'));
     } else {
       res.status(400).send(JSON.stringify('Invalid param string'));

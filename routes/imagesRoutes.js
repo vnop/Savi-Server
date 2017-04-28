ea03a355de3206c0b2e9917a0297c997755ca112
nihilates <nihilates@gmail.com>
2017-04-27 21:32:21 -0400

    Refractor of routes

--- routes/imagesRoutes.js
+++ routes/imagesRoutes.js
@@ -6,8 +6,7 @@ module.exports = function(app, db) {
 
   app.get('/api/images/:imageName', (req, res) => {
     let imageName = req.params.imageName;
-    console.log(path.join(__dirname, '../../Savi-Server/img/'))
-    let exists = fs.existsSync(path.normalize(__dirname, '../../Savi-Server/img/' + imageName));
+    let exists = fs.existsSync(path.join(__dirname, '..', '/img/' + imageName));
     if (imageName && exists) {
       res.sendFile(path.join(__dirname, '/img/' + imageName));
     } else if (!exists) {
